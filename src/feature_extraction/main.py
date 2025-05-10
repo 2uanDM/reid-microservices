ea895b3b4149c7906f9ed50878ba70cd47eaf7d8
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2025-05-10 11:16:04 +0700

    feat/edge's main.py (#3)
    * feat/edge's main.py
    
    * uv install
    
    * fix: main.py
    
    * feat: enhance edge device inferencing
    
    * feat: update Kafka integration and model handling in EdgeDeviceRunner
    
    * docs: add usage instructions for running the edge device
    
    * feat/edge: script sh
    
    * feat: add gen_kafka_compose
    
    * feat: add gen_kafka_compose
    
    * feat: integrate Avro serialization and update dependencies in edge device
    
    ---------
    
    Co-authored-by: 2uanDM <hokage321xxx@gmail.com>
    Co-authored-by: Duong Minh Quan (Justin) <91663460+2uanDM@users.noreply.github.com>
    Co-authored-by: Trần Đức Lê Huy <140135180+amaygoku@users.noreply.github.com>

--- src/feature_extraction/main.py
+++ src/feature_extraction/main.py
@@ -1,6 +1,46 @@
-def main():
-    print("Hello from feature-extraction!")
+import torch
+from fastapi import FastAPI
+from ray import serve
+from torchvision import transforms
+
+from src.models.osnet import osnet_x1_0
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@serve.deployment(
+    num_replicas=4,
+    ray_actor_options={
+        "num_cpus": 1,
+        "num_gpus": 0.25,
+    },  # 1 CPU, 0.25 GPU for each replica
+)
+@serve.ingress(app)  # Attach FastAPI app to the deployment
+class FeatureExtractionService:
+    def __init__(self, img_size: tuple = (128, 64), device: str | None = None):
+        self.device = (
+            device
+            if device is not None
+            else "cuda"
+            if torch.cuda.is_available()
+            else "cpu"
+        )
+        self.model = osnet_x1_0(
+            num_classes=767,
+            loss="softmax",
+            pretrained=True,
+        ).to(self.device)
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((256, 128)),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+        self.model.eval()
+        self.img_size = img_size
+
+    def preprocess(self, img: Image.Image) -> torch.Tensor:
+        pass
